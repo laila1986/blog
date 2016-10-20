000ce794879d74132095df144a3c183152b65a3c
Andee Kaplan <ajkaplan@iastate.edu>
2016-10-20 15:00:28 -0500

    fix typo and blog compiling

--- compile_posts.R
+++ compile_posts.R
@@ -3,10 +3,10 @@ KnitPost <- function(input, base.url = "/") {
   opts_knit$set(base.url = base.url)
   # fig.path <- paste("../images/blog/", sub(".Rmd$", "", basename(input)), "/", sep="")
   # opts_chunk$set(fig.path = fig.path)
-  opts_chunk$set(fig.cap = "center")
+  opts_chunk$set(fig.cap = "center", fig.path = paste0("figure/", strsplit(files, "[.]")[[1]][1], "-"))
   render_jekyll()
   knit(input, envir = parent.frame())
 }
 files <- list.files(".", pattern = "*.Rmd")
 files <- files[files != "LastnameFirstname.Rmd"]
-for(file in files) KnitPost(file, '../../../../')
+for(file in files) KnitPost(file, base.url = "")
